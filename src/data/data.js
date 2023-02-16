9c96ff8e876a521ac108e147ee0707dc8f9c21ba
Jacob-alwin <alwinjacobjoy@gmail.com>
2023-02-16 07:14:56 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,183 @@
+export const data = [
+    {
+        id: 1,
+        buyer:"John Doe",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "1000",
+        deliveryTime: "2 days",
+        postDay: "2 days ago",
+        offer: "10"
+    },
+    {
+        id: 2,
+        buyer:"John Jo",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "600",
+        deliveryTime: "3 days",
+        postDay: "Today",
+        offer: "10"
+    },
+    {
+        id: 3,
+        buyer:"Jacob Doe",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "800",
+        deliveryTime: "2 days",
+        postDay: "2 days ago",
+        offer: "10"
+    },
+    {
+        id: 4,
+        buyer:"John Doe",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "1000",
+        deliveryTime: "2 days",
+        postDay: "2 days ago",
+        offer: "10"
+    },
+    {
+        id: 5,
+        buyer:"John Doe",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "1000",
+        deliveryTime: "2 days",
+        postDay: "2 days ago",
+        offer: "10"
+    },
+    {
+        id: 6,
+        buyer:"John Jo",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "600",
+        deliveryTime: "3 days",
+        postDay: "Today",
+        offer: "10"
+    },
+    {
+        id: 7,
+        buyer:"Jacob Doe",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "800",
+        deliveryTime: "2 days",
+        postDay: "2 days ago",
+        offer: "10"
+    },
+    {
+        id: 8,
+        buyer:"John Doe",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "1000",
+        deliveryTime: "2 days",
+        postDay: "2 days ago",
+        offer: "10"
+    },
+    {
+        id: 9,
+        buyer:"John Doe",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "1000",
+        deliveryTime: "2 days",
+        postDay: "2 days ago",
+        offer: "10"
+    },
+    {
+        id: 10,
+        buyer:"John Jo",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "600",
+        deliveryTime: "3 days",
+        postDay: "Today",
+        offer: "10"
+    },
+    {
+        id: 11,
+        buyer:"Jacob Doe",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "800",
+        deliveryTime: "2 days",
+        postDay: "2 days ago",
+        offer: "10"
+    },
+    {
+        id: 12,
+        buyer:"John Doe",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "1000",
+        deliveryTime: "2 days",
+        postDay: "2 days ago",
+        offer: "10"
+    },
+    {
+        id: 13,
+        buyer:"John Doe",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "1000",
+        deliveryTime: "2 days",
+        postDay: "2 days ago",
+        offer: "10"
+    },
+    {
+        id: 14,
+        buyer:"John Jo",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "600",
+        deliveryTime: "3 days",
+        postDay: "Today",
+        offer: "10"
+    },
+    {
+        id: 15,
+        buyer:"Jacob Doe",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "800",
+        deliveryTime: "2 days",
+        postDay: "2 days ago",
+        offer: "10"
+    },
+    {
+        id: 16,
+        buyer:"John Doe",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "1000",
+        deliveryTime: "2 days",
+        postDay: "2 days ago",
+        offer: "10"
+    },
+    {
+        id: 17,
+        buyer:"John Doe",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "1000",
+        deliveryTime: "2 days",
+        postDay: "2 days ago",
+        offer: "10"
+    },
+    {
+        id: 18,
+        buyer:"John Jo",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "600",
+        deliveryTime: "3 days",
+        postDay: "Today",
+        offer: "10"
+    },
+    {
+        id: 19,
+        buyer:"Jacob Doe",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "800",
+        deliveryTime: "2 days",
+        postDay: "2 days ago",
+        offer: "10"
+    },
+    {
+        id: 20,
+        buyer:"John Doe",
+        projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        price: "1000",
+        deliveryTime: "2 days",
+        postDay: "2 days ago",
+        offer: "10"
+    },
+
+]
